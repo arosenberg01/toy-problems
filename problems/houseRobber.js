f178a4f4a8d9aee9030ca074a76220fed001038b
Ansel Rosenberg <ansel01@gmail.com>
2015-06-23 21:11:22 -0700

    Add houseRobber problem

---
+++ problems/houseRobber.js
@@ -0,0 +1,15 @@
+/**
+ * A burglar is robbing a series of houses and wants to maximize his return, but knows that if he ever robs 
+ * two houses directly next to each other the police will catch him. Write an algorithm to help the robber decide which houses 
+ * to rob from.
+ *
+ * Input: Array of positive integers
+ * Output: Integer
+ * Constraints:
+ *   - Time: O(n)
+ *   - Space: no constraints
+ *
+ * Examples:
+ *   - [5, 25, 15, 30] --> 55
+ *   - [10, 45, 20, 60] --> 70
+ */
