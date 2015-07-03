258cdd9a1236da79447405b04e01d9f51f189dc3
Ansel Rosenberg <ansel01@gmail.com>
2015-07-03 14:37:43 -0700

    Add acyclic linked list problem

---
+++ problems/acyclic-linked-list.js
@@ -0,0 +1,2 @@
+// Determine if a linked list is acyclic - meaning the tail points back at some previous node in the list.
+// The original linked list cannot be modified, nor can additional data structures be used.
