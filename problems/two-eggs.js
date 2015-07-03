7e32f0f17abd392fbb64daae18e1d7586a4f3954
Ansel Rosenberg <ansel01@gmail.com>
2015-07-02 18:33:17 -0700

    Add two-eggs problem

---
+++ problems/two-eggs.js
@@ -0,0 +1,3 @@
+// You are given two identical eggs, and access to a 100-story building.
+// The goal is to find the highest floor from which an egg will not break when dropped
+// out of a window, with the minimum number of egg drops.
