5aa12addf459045cb27ad4a1758255ba5145689b
Ansel Rosenberg <ansel01@gmail.com>
2015-07-13 10:34:31 -0700

    Add build order problem

---
+++ problems/build-order.js
@@ -0,0 +1,9 @@
+/*
+
+You are given a list of projects and a list of dependencies (a list of pairs of
+projects where the first project is dependent on the second project). All of the
+project's dependencies must be built before the project is. Find a valid build 
+order that will allow the project to be build, or return an error if there is
+no valid build order
+
+ */
