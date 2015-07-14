8fb62d9c670367fea9f93b8586203ea41154ff5e
Ansel Rosenberg <ansel01@gmail.com>
2015-07-13 20:35:21 -0700

    Modify build-order problem

--- problems/build-order.js
+++ problems/build-order.js
@@ -2,8 +2,14 @@
 
 You are given a list of projects and a list of dependencies (a list of pairs of
 projects where the first project is dependent on the second project). All of the
-project's dependencies must be built before the project is. Find a valid build 
-order that will allow the project to be build, or return an error if there is
-no valid build order
+projects' dependencies must be built before the project is. Find a valid build 
+order that will allow the projects to be built, or return an error if there is
+no valid build order.
+
+  input:
+    projects: [a, b, c, d, e, f]
+    dependencies: [(d, a), (b, f), (d, b), (a, f), (c, d)]
+  output:
+    [f, e, a, b, d, c]
 
  */
