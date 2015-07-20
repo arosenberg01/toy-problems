2a3ae9e61bc629a193890282dd51a4866f955df9
Ansel Rosenberg <ansel01@gmail.com>
2015-07-19 20:47:06 -0700

    Add asyncMap problem

---
+++ problems/asyncMap.js
@@ -0,0 +1,7 @@
+/*
+
+Write the asyncMap function, which takes in series of async functions that must be run in order,
+and a callback, which is run on an array containing the results of the async tasks (in order) when
+they are all completed.
+
+*/
