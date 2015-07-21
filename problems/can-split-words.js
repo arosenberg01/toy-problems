f6f5b3db23b3e3b9748026209f9b17cac94273bb
Ansel Rosenberg <ansel01@gmail.com>
2015-07-20 20:48:09 -0700

    Add can-split-words problem

---
+++ problems/can-split-words.js
@@ -0,0 +1,18 @@
+/*
+
+Design a function that takes in an array of strings and a target string.
+The function should return true if the string can be split into any sequential
+combination of strings in the array
+
+words = ['at', 'cat', 'dog', 'cats', 'ats']
+s = 'catdog'
+
+--> True
+
+words = ['at', 'cats', 'dog', 'cats', 'ats']
+s = 'catdog'
+
+--> False
+
+
+ */
