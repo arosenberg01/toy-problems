7265df443547a46fc1ccfa49c715272877e53264
Ansel Rosenberg <ansel01@gmail.com>
2015-08-10 10:54:26 -0700

    Add memoized solution for nth fib (python)

--- solutions/python/nth-fib.py
+++ solutions/python/nth-fib.py
@@ -1,15 +1,44 @@
 
-def fib(n):
+def fib_recursive(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib_recursive(n-1) + fib_recursive(n-2)
 
-print(fib(0))
-print(fib(1))
-print(fib(2))
-print(fib(3))
-print(fib(4))
-print(fib(5))
+print(fib_recursive(0))
+print(fib_recursive(1))
+print(fib_recursive(2))
+print(fib_recursive(3))
+print(fib_recursive(4))
+print(fib_recursive(5))
+
+class Fib_Memo:
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n):
+        if n < 0:
+            raise Exception("Index was negative")
+
+        elif n in [0, 1]:
+            return n
+
+        if n in self.memo:
+            return self.memo[n]
+
+        result = self.fib(n-1) + self.fib(n-2)
+
+        self.memo[n] = result
+
+        return result
+
+fib = Fib_Memo()
+# print(fib.fib(0))
+# print(fib.fib(1))
+# print(fib.fib(2))
+# print(fib.fib(3))
+# print(fib.fib(4))
+print(fib.fib(5))
+
