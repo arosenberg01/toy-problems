38d18025f4e13fee9cdff2104dcac7a21e8e0be7
Ansel Rosenberg <ansel01@gmail.com>
2015-08-30 18:05:04 -0700

    Add is-binary-search-tree-problem

---
+++ problems/is-binary-search-tree.js
@@ -0,0 +1,10 @@
+/*
+
+Write a function to check that a binary tree ↴ is a valid binary search tree
+
+A binary search tree is a binary tree in which, for each node:
+
+- The node's value is greater than all values in the left subtree.
+- The node's value is less than all values in the right subtree.
+
+*/
